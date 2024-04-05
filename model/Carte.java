6dc468adc0ec40a3a9e6cf822cec484c8109f4f0
Day5O9 <davidson.charlot@gmail.com>
2024-04-05 09:15:53 +0200

    Affichage de la carte en couleur dans le terminal

--- model/Carte.java
+++ model/Carte.java
@@ -1,9 +1,15 @@
 package model;
+
+import java.util.List;
+import java.util.ArrayList;
 /**
  * 
  */
 public class Carte {
 
+    List<Case> cases = new ArrayList<Case>();
+    // Déclarations des attributs
+    public Case[][] map;
     /**
      * Génère la carte avec la taille, les blocs destructibles et les blocs indestructibles
      */
@@ -16,20 +22,25 @@ public class Carte {
                 // La carte est entourée de blocs indestructibles
                 if (i == 0 || j == 0 || i == n-1 || j == m-1) {
                     map[i][j] = new Case(false, i, j, "BlocIndestructible", false);
+                    cases.add(map[i][j]);
                 }
                 // Les 2 coins opposés sont vides pour laisser la place aux joueurs
                 else if ((i == 1 && j == 1) || (i == 1 && j == 2) || (i == 2 && j == 1) || (i == n-2 && j == m-2) || (i == n-2 && j == m-3) || (i == n-3 && j == m-2)) {
                     map[i][j] = new Case(true, i, j, "CaseVide", false);
+                    cases.add(map[i][j]);
                 }
                 // Les blocs indesctructibles sont placés sur les cases pairs
                 else if (i == 0 || j == 0 || i == n-1 || j == m-1 || (i % 2 == 0 && j % 2 == 0)) {
                     map[i][j] = new Case(false, i, j, "BlocIndestructible", false);
+                    cases.add(map[i][j]);
                 }
                 // Les blocs destructibles sont placés aléatoirement
                 else if (Math.random() < 0.5) {
                     map[i][j] = new Case(false, i, j, "BlocDestructible", true);
+                    cases.add(map[i][j]);
                 } else {
                     map[i][j] = new Case(true, i, j, "CaseVide", false);
+                    cases.add(map[i][j]);
                 }
                 
             }
@@ -39,13 +50,21 @@ public class Carte {
     public void afficherCarte() {
         for (int i = 0; i < map.length ; i++) {
             for (int j = 0; j < map[i].length; j++) {
-                System.out.print(map[i][j].estTraversable + "\t");
+                if(map[i][j].estTraversable && !map[i][j].estDestructible){
+                    System.out.print("\u001B[34m " + "T" + "\t" + "\u001B[0m");
+                }
+                else if(!map[i][j].estTraversable && map[i][j].estDestructible){
+                    System.out.print("\u001B[32m " + "D" + "\t" + "\u001B[0m");
+                }
+                else if(!map[i][j].estTraversable && !map[i][j].estDestructible){
+                    System.out.print("\u001B[31m " + "M" + "\t" + "\u001B[0m");
+                }
+                else{
+                    System.out.print("V" + "\t");
+                }
             }
             System.out.println();
         }
     }
-
-    // Déclarations des attributs
-    public Case[][] map;
 }
 
