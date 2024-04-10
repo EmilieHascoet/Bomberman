b15d0106f863618a94f57dfb581d4cd954688e9f
Day5O9 <davidson.charlot@gmail.com>
2024-04-11 00:08:28 +0200

    Possibilité de faire des choix multiple et de les enregistrer

--- view/ParametresPanel.java
+++ view/ParametresPanel.java
@@ -1,5 +1,7 @@
 package view;
 
+import model.Bomberman;
+
 import javax.swing.JPanel;
 import javax.swing.JButton;
 import java.awt.Color;
@@ -12,26 +14,35 @@ import java.awt.Font;
 import javax.swing.DefaultListModel;
 import javax.swing.JList;
 import javax.swing.JCheckBox;
+import javax.swing.JDialog;
+import javax.swing.JTextField;
 import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
 import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 import javax.swing.ListSelectionModel;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
+import javax.swing.JOptionPane;
 
 import controller.RetourController;
 
 import java.awt.Insets;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 public class ParametresPanel extends JPanel {
     private MainFrame frame;
     private JPanel rightPanel;
     private CardLayout cardLayout;
+    private Bomberman b;
 
-    public ParametresPanel(MainFrame frame) {
+    public ParametresPanel(MainFrame frame, Bomberman bomberman) {
         this.frame = frame;
         setLayout(new GridBagLayout());
         setBackground(Color.white);
+        b = bomberman;
 
         JPanel squarePanel = new JPanel();
         squarePanel.setLayout(new GridLayout(1, 2));
@@ -50,9 +61,11 @@ public class ParametresPanel extends JPanel {
 
         DefaultListModel<String> listModel = new DefaultListModel<>();
         JList<String> list = new JList<>(listModel);
-        listModel.addElement("Label 1");
-        listModel.addElement("Label 2");
-        listModel.addElement("Label 3");
+        listModel.addElement("Liste de bonus");
+        listModel.addElement("Partie");
+        listModel.addElement("Joueur 1");
+        listModel.addElement("Joueur 2");
+
         list.setFont(new Font("Arial", Font.PLAIN, 16));
         list.setForeground(Color.black);
         list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION); // Allow only single selection
@@ -85,11 +98,17 @@ public class ParametresPanel extends JPanel {
         rightPanel.setBackground(Color.white);
 
         // Create different sets of checkboxes for each label
-        createCheckBoxes("Label 1", rightPanel);
-        createCheckBoxes("Label 2", rightPanel);
-        createCheckBoxes("Label 3", rightPanel);
 
-        // Add the rightPanel to the squarePanel
+        // Create checkboxes for each label
+        createCheckBoxes(listModel.get(0).toString(), rightPanel,
+                b.parametres.getListBonus().toArray(new String[b.parametres.getListBonus().size()]));
+        createTextAreas(listModel.get(1).toString(), rightPanel, new String[] {
+                "Nombre de vies", "Vitesse", "Nombre de bombes initiales", "Portée de la bombe", "Largeur du plateau",
+                "Hauteur du plateau"
+        });
+        createTextAreas(listModel.get(2), rightPanel, new String[] { "Nom du joueur 1", "Couleur du joueur 1" });
+        createTextAreas(listModel.get(3), rightPanel, new String[] { "Nom du joueur 2", "Couleur du joueur 2" });
+
         squarePanel.add(rightPanel);
 
         // Bottom panel with a button
@@ -99,9 +118,144 @@ public class ParametresPanel extends JPanel {
         retour.setForeground(Color.white);
         retour.setBackground(new Color(51, 153, 255));
 
+        JButton valider = new JButton("Valider") {
+            {
+                Set<String> listBonus = new HashSet<>();
+                String [] l = new String[] {"Nombre de vies", "Vitesse", "Nombre de bombes initiales", "Portée de la bombe", "Largeur du plateau", "Hauteur du plateau"};
+                Map<String, String> list = new java.util.HashMap<>();
+                Map<String, String> list2 = new java.util.HashMap<>();
+                
+                addActionListener(e -> {
+                    // Get the selected checkboxes
+                    for (int i = 0; i < b.parametres.getListBonus().size(); i++) {
+                        JCheckBox checkBox = (JCheckBox) ((JPanel) rightPanel.getComponent(0)).getComponent(i);
+                        if (checkBox.isSelected()) {
+                            listBonus.add(checkBox.getText());
+                        } else {
+                            listBonus.remove(checkBox.getText());
+                        }
+                    }
+                    if (listBonus.isEmpty()) {
+                        JOptionPane.showMessageDialog(this, "Veuillez sélectionner au moins un bonus.", "Erreur",
+                                JOptionPane.ERROR_MESSAGE);
+                        return;
+                    }
+                    else{
+                        b.parametres.setListBonus(listBonus);
+                    }
+
+                    // Get the text areas
+                    for (int i = 0; i < l.length; i++) {
+                        JTextField textArea = (JTextField) ((JPanel) rightPanel.getComponent(1)).getComponent(i);
+                        if (textArea.getText().equals(list.get(textArea.getText().toString()))) {
+                            JOptionPane.showMessageDialog(this, "Veuillez remplir tous les champs.", "Erreur",
+                                    JOptionPane.ERROR_MESSAGE);
+                            return;
+                        }
+                        else{
+                            switch (i) {
+                                case 0:
+                                    list.put(l[0], textArea.getText());
+                                case 1:
+                                    list.put(l[1], textArea.getText());
+                                case 2:
+                                    list.put(l[2], textArea.getText());
+                                case 3:
+                                    list.put(l[3], textArea.getText());
+                                case 4:
+                                    list.put(l[4], textArea.getText());
+                                case 5:
+                                    list.put(l[5], textArea.getText());
+                                    break;
+                                default:
+                                    break;
+                            }
+                        }
+                    }
+
+                    for (int i = 0; i < 2; i++) {
+                        JTextField textArea = (JTextField) ((JPanel) rightPanel.getComponent(2)).getComponent(i);
+                        if (textArea.getText().equals(list2.get(textArea.getText().toString()))) {
+                            JOptionPane.showMessageDialog(this, "Veuillez remplir tous les champs.", "Erreur",
+                                    JOptionPane.ERROR_MESSAGE);
+                            return;
+                        }
+                        else{
+                            switch (i) {
+                                case 0:
+                                    list2.put("Nom du joueur 1", textArea.getText());
+                                case 1:
+                                    list2.put("Couleur du joueur 1", textArea.getText());
+                                    break;
+                                default:
+                                    break;
+                            }
+                        }
+                    }
+
+                    for (int i = 0; i < 2; i++) {
+                        JTextField textArea = (JTextField) ((JPanel) rightPanel.getComponent(3)).getComponent(i);
+                        if (textArea.getText().equals(list2.get(textArea.getText().toString()))) {
+                            JOptionPane.showMessageDialog(this, "Veuillez remplir tous les champs.", "Erreur",
+                                    JOptionPane.ERROR_MESSAGE);
+                            return;
+                        }
+                        else{
+                            switch (i) {
+                                case 0:
+                                    list2.put("Nom du joueur 2", textArea.getText());
+                                case 1:
+                                    list2.put("Couleur du joueur 2", textArea.getText());
+                                    break;
+                                default:
+                                    break;
+                            }
+                        }
+                    }
+
+                    
+                    
+                    JDialog dialog = new JDialog();
+                    dialog.setAlwaysOnTop(true);
+                    dialog.setModal(true);
+                    dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+                    dialog.setTitle("Paramètres");
+                    dialog.setSize(400, 300);
+                    dialog.setLocationRelativeTo(null);
+                    dialog.add(new JPanel() {
+                        {
+                            setBackground(Color.white);
+                            add(new JTextArea(listBonus.toString().format("Liste de bonus : %s", listBonus) + "\n" + list.toString().format("Partie : %s", list)) {
+                                {
+                                    setFont(new Font("Arial", Font.PLAIN, 16));
+                                    setForeground(Color.black);
+                                    setEditable(false);
+                                    setBackground(Color.white);
+                                    setLineWrap(true);
+                                    setWrapStyleWord(true);
+                                    
+                                }
+                            });
+                            add(new JButton("OK") {
+                                {
+                                    addActionListener(e -> dialog.dispose());
+                                }
+                            });
+                        
+                        }
+                    });
+                    dialog.setVisible(true);
+                });
+            }
+        };
+        valider.setFont(new Font("Arial", Font.BOLD, 16));
+        valider.setForeground(Color.white);
+        valider.setBackground(new Color(51, 153, 255));
+
         JPanel bottomPanel = new JPanel();
         bottomPanel.setBackground(Color.white);
         bottomPanel.add(retour);
+        bottomPanel.add(valider);
 
         GridBagConstraints gbc = new GridBagConstraints();
         gbc.gridx = 0;
@@ -123,22 +277,71 @@ public class ParametresPanel extends JPanel {
     }
 
     // Method to create checkboxes for a specific label
-    private void createCheckBoxes(String label, JPanel parentPanel) {
-        JPanel checkBoxPanel = new JPanel();
+    private void createCheckBoxes(String label, JPanel parentPanel, String[] list) {
+        JPanel checkBoxPanel = new JPanel(new GridLayout(0, 2)); // 2 colonnes
         checkBoxPanel.setBackground(Color.white);
-        checkBoxPanel.setLayout(new BoxLayout(checkBoxPanel, BoxLayout.Y_AXIS));
 
-        JCheckBox checkbox1 = new JCheckBox(label + " Option 1");
-        JCheckBox checkbox2 = new JCheckBox(label + " Option 2");
-        JCheckBox checkbox3 = new JCheckBox(label + " Option 3");
-        // Style checkboxes
-        checkbox1.setFont(new Font("Arial", Font.PLAIN, 14));
-        checkbox2.setFont(new Font("Arial", Font.PLAIN, 14));
-        checkbox3.setFont(new Font("Arial", Font.PLAIN, 14));
-        checkBoxPanel.add(checkbox1);
-        checkBoxPanel.add(checkbox2);
-        checkBoxPanel.add(checkbox3);
+        for (String item : list) {
+            JCheckBox checkBox = new JCheckBox(item);
+            checkBox.setFont(new Font("Arial", Font.PLAIN, 16));
+            checkBox.setForeground(Color.black);
+            checkBoxPanel.add(checkBox);
+        }
 
         parentPanel.add(checkBoxPanel, label);
     }
+
+    private void createTextAreas(String label, JPanel parentPanel, String[] list) {
+        JPanel textAreaPanel = new JPanel();
+        textAreaPanel.setBackground(Color.white);
+        textAreaPanel.setLayout(new BoxLayout(textAreaPanel, BoxLayout.Y_AXIS));
+
+        JTextField[] textAreas = new JTextField[list.length];
+
+        for (int j = 0; j < list.length; j++) {
+            final int i = j;
+            textAreas[i] = new JTextField(list[i]) {
+                {
+                    addFocusListener(new java.awt.event.FocusAdapter() {
+                        public void focusGained(java.awt.event.FocusEvent evt) {
+                            jTextFieldFocusGained(evt);
+                        }
+
+                        public void focusLost(java.awt.event.FocusEvent evt) {
+                            jTextFieldFocusLost(evt);
+                        }
+                    });
+                }
+
+                private void jTextFieldFocusGained(java.awt.event.FocusEvent evt) {
+                    if (getText().equals(list[i])) {
+                        setText("");
+                        setForeground(Color.black);
+                    }
+                }
+
+                private void jTextFieldFocusLost(java.awt.event.FocusEvent evt) {
+                    if (getText().equals("")) {
+                        setText(list[i]);
+                        setForeground(new Color(153, 153, 153));
+                    }
+                    /*
+                     * String text = this.getText();
+                     * if (!text.matches("\\d*")) { // Vérifie si la chaîne ne contient que des
+                     * chiffres
+                     * JOptionPane.showMessageDialog(this,
+                     * "Veuillez entrer uniquement des chiffres.", "Erreur",
+                     * JOptionPane.ERROR_MESSAGE);
+                     * }
+                     */
+                }
+            };
+            textAreas[i].setFont(new Font("Arial", Font.PLAIN, 16));
+            textAreas[i].setForeground(new Color(153, 153, 153));
+            textAreaPanel.add(textAreas[i]);
+        }
+
+        parentPanel.add(textAreaPanel, label);
+    }
+
 }
