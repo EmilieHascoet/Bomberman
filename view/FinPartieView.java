3d82fd95ab33dc5ca9304d5ae3e34e45ebe9f5be
Hossein Motalebi <hosseinabdolmotalebi@gmail.com>
2024-05-13 21:12:08 +0200

    mise à jour avec dialog et les méthode statics

--- view/FinPartieView.java
+++ view/FinPartieView.java
@@ -1,53 +1,52 @@
 package view;
 
 import javax.swing.*;
-
 import java.awt.*;
 import java.awt.event.ActionListener;
-import controller.FinPartieController;
 
-public class FinPartieView extends JFrame {
+import model.Partie;
 
-    private JButton buttonExit;
+public class FinPartieView extends JDialog {
     private JButton buttonRejouer;
     private JButton buttonMenu;
+    private JButton buttonExit;
 
-    public FinPartieView(int scorePlayer1, int scorePlayer2, int nbBombesUtilisees, boolean isPlayer1Winner) {
-        // Configuration initiale...
-        super("Fin de Partie");
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    public FinPartieView(Frame owner) {
+        super(owner, "Fin de Partie", true);
+        setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
         setSize(400, 220);
         setLocationRelativeTo(null);
         setLayout(new GridBagLayout());
         GridBagConstraints gbc = new GridBagConstraints();
         gbc.insets = new Insets(10, 10, 10, 10);
 
-        // Ajout des composants...
-        setupComponents(gbc, scorePlayer1, scorePlayer2, nbBombesUtilisees, isPlayer1Winner);
+        int scorePlayer1 = Partie.getJoueurs().get(0).score;
+        int scorePlayer2 = Partie.getJoueurs().get(1).score;
+        boolean isPlayer1Winner = scorePlayer1 > scorePlayer2;
 
+        setupComponents(gbc, scorePlayer1, scorePlayer2, isPlayer1Winner);
         setVisible(true);
     }
 
-    private void setupComponents(GridBagConstraints gbc, int scorePlayer1, int scorePlayer2, int nbBombesUtilisees,
-            boolean isPlayer1Winner) {
+    private void setupComponents(GridBagConstraints gbc, int scorePlayer1, int scorePlayer2, boolean isPlayer1Winner) {
         JLabel labelGagnant = new JLabel(isPlayer1Winner ? "Player 1 gagne !" : "Player 2 gagne !");
         labelGagnant.setForeground(isPlayer1Winner ? Color.BLUE : Color.RED);
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.gridwidth = 3;
         add(labelGagnant, gbc);
 
         gbc.gridwidth = 1;
-        gbc.gridx = 1;
         gbc.gridy = 1;
         add(new JLabel("Score Player 1: " + scorePlayer1), gbc);
-        gbc.gridy = 2;
+        gbc.gridx = 2;
         add(new JLabel("Score Player 2: " + scorePlayer2), gbc);
-        gbc.gridy = 3;
-        add(new JLabel("Nb. bombes utilisées: " + nbBombesUtilisees), gbc);
 
-        gbc.gridy = 4;
+        gbc.gridy = 2;
+        gbc.gridx = 0;
         gbc.fill = GridBagConstraints.HORIZONTAL;
 
         buttonExit = new JButton("Sortir");
-        gbc.gridx = 0;
         add(buttonExit, gbc);
 
         buttonRejouer = new JButton("Rejouer");
@@ -59,21 +58,15 @@ public class FinPartieView extends JFrame {
         add(buttonMenu, gbc);
     }
 
-    public void addExitButtonListener(ActionListener listener) {
-        buttonExit.addActionListener(listener);
-    }
-
-    public void addReplayButtonListener(ActionListener listener) {
+    public void setRejouerAction(ActionListener listener) {
         buttonRejouer.addActionListener(listener);
     }
 
-    public void addMenuButtonListener(ActionListener listener) {
+    public void setMenuAction(ActionListener listener) {
         buttonMenu.addActionListener(listener);
     }
 
-    public static void main(String[] args) {
-        // Création de la fenêtre de fin de partie avec des valeurs de test.
-        SwingUtilities.invokeLater(() -> new FinPartieController(new FinPartieView(3, 5, 10, true)));
-
+    public void setExitAction(ActionListener listener) {
+        buttonExit.addActionListener(listener);
     }
 }
