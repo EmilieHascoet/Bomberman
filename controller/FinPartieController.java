2b6eabaef3dc0e18196a5e2a8e30ee34cc3e7e02
Hossein Motalebi <hosseinabdolmotalebi@gmail.com>
2024-05-14 00:32:28 +0200

    FinPartieView et controller testée et fonctionnel

--- controller/FinPartieController.java
+++ controller/FinPartieController.java
@@ -1,16 +1,21 @@
 package controller;
 
 import view.*;
+import model.Main;
 import model.Partie;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class FinPartieController {
     private FinPartieView view;
+    private MainFrame mainFrame;
 
     public FinPartieController(FinPartieView view) {
         this.view = view;
+        mainFrame = view.getMainFrame();
         initController();
+        view.display();
+
     }
 
     private void initController() {
@@ -18,22 +23,27 @@ public class FinPartieController {
             @Override
             public void actionPerformed(ActionEvent e) {
                 // rejouer avec les mêmes paramêtres
+                view.dispose();
                 Partie.lancerNouvellePartie();
+                PartiePanel partiePanel = new PartiePanel(mainFrame);
+                mainFrame.changePanel(partiePanel);
+                partiePanel.requestFocusInWindow();
             }
         });
 
         view.setMenuAction(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                // retourner à l'accueil
+                view.dispose();
                 MainFrame.main(null);
+                // retourner à l'accueil
             }
         });
 
         view.setExitAction(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                view.dispose(); // Fermer la fenêtre
+                System.exit(0); // Fermer la fenêtre
             }
         });
     }
